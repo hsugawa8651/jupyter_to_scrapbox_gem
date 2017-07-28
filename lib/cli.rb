4cad993df4d9d80cd136ad1a6d82a3eb1b8c453d
Hiroharu Sugawara <hsugawa@tmu.ac.jp>
2017-07-28 22:02:35 +0900

    cli.rb added

---
+++ lib/cli.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require "jupyter_to_scrapbox"
+require 'thor'
+
+module JupyterToScrapbox
+  class CLI < Thor
+    class_option :help, type: :boolean, aliases: '-h', desc: 'help message.'
+    class_option :version, type: :boolean, desc: 'version'
+    default_task :help
+
+    method_option :verbose, type: :boolean, aliases: '-v', desc: 'verbose message', :default => false
+    method_option :image, type: :boolean, aliases: '-i', desc: 'register images', :default => false
+    method_option :markdown, type: :boolean, aliases: '-m', desc: 'process markdown', :default => true
+    desc 'converts FILES [options]', 'Convert jupyter-notebook files to scrapbox-json'
+
+    def convert(*paths)
+      JupyterToScrapbox::Converter.set_verbose(options[:verbose])
+      JupyterToScrapbox::Converter.set_register_images(options[:image])
+      JupyterToScrapbox::Converter.set_parse_markdown(options[:markdown])
+      paths.each do |path|
+        JupyterToScrapbox::Converter.add(path)
+      end
+      JupyterToScrapbox::Converter.perform()
+    end
+
+    desc 'version', 'version'
+    def version
+      p JupyterToScrapbox::VERSION
+    end
+  end
+end
--- lib/jupyter_to_scrapbox/version.rb
+++ lib/jupyter_to_scrapbox/version.rb
@@ -1,3 +1,3 @@
 module JupyterToScrapbox
-  VERSION = "0.2.2"
+  VERSION = "0.2.3"
 end
